380d7583f18bd997532ca6215ad902c9488379e7
Dave <Dwtmekonnen123@gmail.com>
2023-08-21 11:28:12 +0300

    tailwind configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [tailwind()]
 });
