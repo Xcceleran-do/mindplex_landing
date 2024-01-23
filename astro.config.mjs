9019d90c4abd9a0148e65b0fab70ad1cee4a94cd
Tensae <tensaeb2016@gmail.com>
2024-01-23 14:48:03 +0300

    google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,18 @@ import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		})
+  ]
 });
